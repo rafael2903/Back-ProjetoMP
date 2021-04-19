542285a82fe649ff8cd1ee7adb0097511df94d43
priscila-eng <priscilaangell123@gmail.com>
2021-04-19 12:00:51 -0300

    Cria tabela e CRUD de formulario

---
+++ app/controllers/forms_controller.rb
@@ -0,0 +1,64 @@
+class FormsController < ApplicationController
+  before_action :set_form, only: [:show, :update, :destroy]
+
+  # GET /forms
+  def index
+    @forms = Form.all
+    @forms.each do |form|
+      my_xml = form.question
+      form.question = Hash.from_xml(my_xml).to_json
+    end
+    render json: @forms
+  end
+
+  # GET /forms/1
+  def show
+    my_xml = @form.question
+    @form.question = Hash.from_xml(my_xml).to_json
+    render json: @form
+  end
+
+  # POST /forms
+  def create
+    my_json = params[:question]
+    my_json2 = (my_json).to_json
+    my_xml = JSON.parse(my_json2).to_xml
+
+    @form = Form.new({:question => my_xml})
+
+    if @form.save
+      render json: @form, status: :created, location: @form
+    else
+      render json: @form.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /forms/1
+  def update
+    my_json = params[:question]
+    my_json2 = (my_json).to_json
+    my_xml = JSON.parse(my_json2).to_xml
+
+    if @form.update(form_params)
+      render json: @form
+    else
+      render json: @form.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /forms/1
+  def destroy
+    @form.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_form
+      @form = Form.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def form_params
+      params.require(:form).permit(:question, :id)
+    end
+end
---
+++ db/migrate/20210417222907_create_forms.rb
@@ -0,0 +1,9 @@
+class CreateForms < ActiveRecord::Migration[6.0]
+  def change
+    create_table :forms do |t|
+      t.string :question
+
+      t.timestamps
+    end
+  end
+end
