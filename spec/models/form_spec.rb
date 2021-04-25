c8187e8fb103e9991dc0e04c07ca74ac48599225
priscila-eng <priscilaangell123@gmail.com>
2021-04-25 19:19:09 -0300

    Conserta erros de estilo de codigo do rubocop

--- spec/models/form_spec.rb
+++ spec/models/form_spec.rb
@@ -1,5 +1,7 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-RSpec.describe Form, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# require 'rails_helper'
+
+# RSpec.describe Form, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
--- spec/routing/forms_routing_spec.rb
+++ spec/routing/forms_routing_spec.rb
@@ -1,30 +1,31 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe FormsController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(get: "/forms").to route_to("forms#index")
+  describe 'roteamento' do
+    it 'rotas para o método #index' do
+      expect(get: '/forms').to route_to('forms#index')
     end
 
-    it "routes to #show" do
-      expect(get: "/forms/1").to route_to("forms#show", id: "1")
+    it 'rotas para o método #show' do
+      expect(get: '/forms/1').to route_to('forms#show', id: '1')
     end
 
-
-    it "routes to #create" do
-      expect(post: "/forms").to route_to("forms#create")
+    it 'rotas para o método #create' do
+      expect(post: '/forms').to route_to('forms#create')
     end
 
-    it "routes to #update via PUT" do
-      expect(put: "/forms/1").to route_to("forms#update", id: "1")
+    it 'rotas para o método #update via PUT' do
+      expect(put: '/forms/1').to route_to('forms#update', id: '1')
     end
 
-    it "routes to #update via PATCH" do
-      expect(patch: "/forms/1").to route_to("forms#update", id: "1")
+    it 'rotas para o método #update via PATCH' do
+      expect(patch: '/forms/1').to route_to('forms#update', id: '1')
     end
 
-    it "routes to #destroy" do
-      expect(delete: "/forms/1").to route_to("forms#destroy", id: "1")
+    it 'rotas para o método #destroy' do
+      expect(delete: '/forms/1').to route_to('forms#destroy', id: '1')
     end
   end
 end
