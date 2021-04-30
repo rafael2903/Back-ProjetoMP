3922060762b519bad3b712788201047108d14e91
priscila-eng <priscilaangell123@gmail.com>
2021-04-30 15:10:29 -0300

    Adiciona xml

--- app/controllers/form_answers_controller.rb
+++ app/controllers/form_answers_controller.rb
@@ -4,32 +4,56 @@ class FormAnswersController < ApplicationController
   # GET /form_answers
   def index
     @form_answers = FormAnswer.all
-
+    @form_answer.each do |form|
+      my_xml = form.answers
+      type = my_xml.kind_of? String
+      if type === false
+        form.answers = Hash.from_xml(my_xml).to_json
+      end
+    end
     render json: @form_answers
   end
 
   # GET /form_answers/1
   def show
+    my_xml = @form.answers
+    type = my_xml.kind_of? String
+    if type === false
+      form.answers = Hash.from_xml(my_xml).to_json
+    end
     render json: @form_answer
   end
 
   # POST /form_answers
   def create
-    @form_answer = FormAnswer.new(form_answer_params)
-
-    if @form_answer.save
-      render json: @form_answer, status: :created, location: @form_answer
-    else
-      render json: @form_answer.errors, status: :unprocessable_entity
+    my_json = params[:answers]
+    type = my_json.kind_of? Array
+    if type === true
+      my_json2 = my_json.to_json
+      my_xml = JSON.parse(my_json2).to_xml
+      @form_answer = FormAnswer.new({answers: my_xml, user_id:
+      params[:user_id], form_id: params[:form_id]})
+      if @form_answer.save
+        render json: @form_answer, status: :created, location: @form_answer
+      else
+        render json: @form_answer.errors, status: :unprocessable_entity
+      end
     end
   end
 
   # PATCH/PUT /form_answers/1
   def update
-    if @form_answer.update(form_answer_params)
-      render json: @form_answer
-    else
-      render json: @form_answer.errors, status: :unprocessable_entity
+    my_json = params[:answers]
+    type = my_json.kind_of? Array
+    if type === true
+      my_json2 = my_json.to_json
+      my_xml = JSON.parse(my_json2).to_xml
+      if @form_answer.update({answers: my_xml, user_id:
+      params[:user_id], form_id: params[:form_id]})
+        render json: @form_answer
+      else
+        render json: @form_answer.errors, status: :unprocessable_entity
+      end
     end
   end
 
@@ -39,13 +63,11 @@ class FormAnswersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_form_answer
       @form_answer = FormAnswer.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def form_answer_params
-      params.require(:form_answer).permit(:answers)
+      params.require(:form_answer).permit(:answers, :form_id, :user_id)
     end
 end
